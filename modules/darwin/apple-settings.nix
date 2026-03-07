974975a078cbde10b220475d14605ac988c7835d
jackson <jacksonwc@proton.me>
2026-03-06 18:23:38 -0600

    more macos

--- modules/darwin/apple-settings.nix
+++ modules/darwin/apple-settings.nix
@@ -1,5 +1,4 @@
-{ ... }:
-{
+{...}: {
   system.defaults = {
     NSGlobalDomain = {
       AppleICUForce24HourTime = true;
@@ -7,11 +6,17 @@
       KeyRepeat = 2;
       AppleShowAllExtensions = true;
       NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      ApplePressAndHoldEnabled = false;
     };
 
     finder = {
       AppleShowAllFiles = true;
       ShowPathbar = true;
+      ShowStatusBar = true;
+      FXPreferedViewStyle = "Nlsv";
+      _FXSortFoldersFirst = true;
+      FXEnableExtensionChangeWarning = false;
     };
 
     dock = {
@@ -20,16 +25,20 @@
       autohide-time-modifier = 0.0;
     };
 
+    trackpad = {
+      Clicking = true;
+    };
+
     CustomUserPreferences = {
       "com.apple.symbolichotkeys" = {
         AppleSymbolicHotKeys = {
-          "28" = { enabled = 0; };
-          "29" = { enabled = 0; };
-          "30" = { enabled = 0; };
-          "31" = { enabled = 0; };
-          "64" = { enabled = 0; };
-          "65" = { enabled = 0; };
-          "184" = { enabled = 0; };
+          "28" = {enabled = 0;};
+          "29" = {enabled = 0;};
+          "30" = {enabled = 0;};
+          "31" = {enabled = 0;};
+          "64" = {enabled = 0;};
+          "65" = {enabled = 0;};
+          "184" = {enabled = 0;};
         };
       };
     };
