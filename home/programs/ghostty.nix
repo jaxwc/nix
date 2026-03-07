7caa31e91e215c92e99f286520c298c08d0b9f16
jackson <jacksonwc@proton.me>
2026-03-07 17:20:26 -0600

    moved ghostty confix to be managed by nix

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -1,6 +1,30 @@
-{ ... }:
-{
+{theme, ...}: {
   xdg.configFile."ghostty/config" = {
-    source = ../config/ghostty/config;
+    text = ''
+      auto-update-channel = tip
+
+      font-family = JetBrainsMono Nerd Font
+      font-size = 16
+
+      theme = ${theme.ghosttyTheme}
+      background-opacity = 0.70
+      background-blur = true
+      background = ${theme.colors.black}
+
+      window-padding-x = 8
+      window-padding-y = 8
+      window-padding-balance = true
+
+      cursor-style = block
+      cursor-invert-fg-bg = true
+
+      mouse-hide-while-typing = true
+      macos-titlebar-style = hidden
+      macos-option-as-alt = true
+
+      shell-integration-features = no-cursor
+
+      keybind = super+shift+r=reload_config
+    '';
   };
 }
