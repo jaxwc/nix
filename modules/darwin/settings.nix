4835eb443ef44db2a864362be7f62aa8c1051954
jackson <jacksonwc@proton.me>
2026-04-04 12:14:52 -0500

    update

--- modules/darwin/settings.nix
+++ modules/darwin/settings.nix
@@ -1,5 +1,8 @@
-{ pkgs, user, ... }:
 {
+  pkgs,
+  user,
+  ...
+}: {
   nix.enable = false;
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = "aarch64-darwin";
@@ -9,13 +12,17 @@
     shell = pkgs.fish;
   };
   programs.fish.enable = true;
-  environment.shells = [ pkgs.fish ];
+  environment.shells = [pkgs.fish];
   environment.systemPackages = with pkgs; [
     curl
   ];
 
-  launchd.user.envVariables.PATH =
-    "/etc/profiles/per-user/${user}/bin:/run/current-system/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+  launchd.user.envVariables.PATH = "/etc/profiles/per-user/${user}/bin:/run/current-system/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+
+  security.pam.services.sudo_local = {
+    touchIdAuth = true;
+    reattach = true;
+  };
 
   system.stateVersion = 6;
 }
