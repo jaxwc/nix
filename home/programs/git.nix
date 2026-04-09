51af033420a8731d5a1cc17f72b5ead83badfbd7
jackson <jacksonwc@proton.me>
2026-04-09 15:09:04 -0500

    fixed sign with git

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   programs.git = {
     enable = true;
     signing = {
@@ -11,6 +11,8 @@
       user.name = "jackson";
       user.email = "jacksonwc@proton.me";
 
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+
       init.defaultBranch = "main";
       fetch.prune = true;
       push.autoSetupRemote = true;
@@ -22,4 +24,8 @@
       diff.colorMoved = "default";
     };
   };
+
+  home.file.".ssh/allowed_signers".text = ''
+    jacksonwc@proton.me namespaces="git" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUfVjN3hYmmXaDlfw0si0aKKHnjViSW5fQ5KynxVcZC
+  '';
 }
