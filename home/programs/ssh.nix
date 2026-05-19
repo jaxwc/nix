d5ea4f0fef505869d45bc31fcd6eaaf5c9383772
jackson <jacksonwc@proton.me>
2026-05-18 23:29:01 -0500

    update

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -8,25 +8,22 @@ lib.mkIf pkgs.stdenv.isDarwin {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = true;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
-
-      extraOptions = {
-        IdentityAgent = "\"${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
-      };
+    settings."*" = {
+      ForwardAgent = false;
+      AddKeysToAgent = "no";
+      Compression = false;
+      ServerAliveInterval = 0;
+      ServerAliveCountMax = 3;
+      HashKnownHosts = true;
+      UserKnownHostsFile = "~/.ssh/known_hosts";
+      ControlMaster = "no";
+      ControlPath = "~/.ssh/master-%r@%n:%p";
+      ControlPersist = "no";
+      IdentityAgent = "\"${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
     };
-    matchBlocks."github.com" = {
-      hostname = "github.com";
-      user = "git";
+    settings."github.com" = {
+      HostName = "github.com";
+      User = "git";
     };
   };
 }
--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -11,7 +11,6 @@
       "1Password for Safari" = 1569813296;
       "Obsidian Web Clipper" = 6720708363;
       "wBlock" = 6746388723;
-      "Noir - Dark Mode for Safari" = 1592917505;
     };
     casks = [
       "1password@beta"
