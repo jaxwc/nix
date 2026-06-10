66c40e30a40136b8207eed0aaaa989b473141ec1
jackson <jacksonwc@proton.me>
2026-06-10 09:30:54 -0500

    idek

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -4,6 +4,7 @@
     onActivation.autoUpdate = true;
     onActivation.upgrade = true;
     onActivation.cleanup = "zap";
+    onActivation.extraFlags = ["--force-cleanup"];
     brews = [
       "mas"
     ];
